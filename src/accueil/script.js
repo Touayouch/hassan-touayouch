02627495ee600106ba599736ba094e561057bd11
Touayouch <hassantoua19@gmail.com>
2026-05-12 00:04:16 +0200

    Refactor script.js for improved readability and structure

--- src/accueil/script.js
+++ src/accueil/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function() 
     const loading = document.getElementById('loading');
     setTimeout(() => {
         loading.style.opacity = '0';
