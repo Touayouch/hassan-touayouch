b8443027c159820d780a39104867ea01f66a7376
Touayouch <hassantoua19@gmail.com>
2026-05-18 23:53:28 +0200

    Refactor script.js for improved loading and menu handling

--- src/accueil/script.js
+++ src/accueil/script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
         setTimeout(() => {
             loading.style.display = 'none';
         }, 400);
-    }, 400);
+    }, 500);
 });
 
 function toggleMenu() {
